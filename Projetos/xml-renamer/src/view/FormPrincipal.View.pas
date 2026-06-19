5f01c796278573e333a2d8466c324dcdebf14e90
Belmiro Neto <netobelmiro1@gmail.com>
2026-06-18 21:09:19 -0300

    feat: adiciona implementação dos botões analisar e renomear e metodo de logs

--- Projetos/xml-renamer/src/view/FormPrincipal.View.pas
+++ Projetos/xml-renamer/src/view/FormPrincipal.View.pas
@@ -5,7 +5,8 @@ interface
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
   Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ExtCtrls, Vcl.StdCtrls,   Vcl.FileCtrl,
-  XmlScanner.Service, System.Generics.Collections, XmlInfo.Model;
+  XmlScanner.Service, System.Generics.Collections, XmlInfo.Model,
+  XmlRename.Service, System.ImageList, Vcl.ImgList;
 
 type
   TfrmPrincipal = class(TForm)
@@ -16,12 +17,16 @@ type
     btnAnalisar: TButton;
     btnRenomear: TButton;
     Label1: TLabel;
+    ImageList1: TImageList;
     procedure btnProcurarPastaClick(Sender: TObject);
     procedure FormCreate(Sender: TObject);
     procedure FormDestroy(Sender: TObject);
     procedure btnAnalisarClick(Sender: TObject);
+    procedure Log(const AMensagem: string);
+    procedure btnRenomearClick(Sender: TObject);
   private
     FScannerService : TXmlScannerService;
+    FRenameService : TXmlRenameService;
     FListaXmls: TObjectList<TXmlInfo>;
 
   public
@@ -38,15 +43,26 @@ implementation
 procedure TfrmPrincipal.FormCreate(Sender: TObject);
 begin
  FScannerService := TXmlScannerService.Create;
+ FRenameService := TXmlRenameService.Create;
 end;
 
 procedure TfrmPrincipal.FormDestroy(Sender: TObject);
 begin
  FListaXmls.Free;
  FScannerService.Free;
+ FRenameService.Free;
 end;
 
 
+
+procedure TfrmPrincipal.Log(const AMensagem: string);
+begin
+  mmLog.Lines.Add(
+    FormatDateTime('hh:nn:ss', Now) +
+    ' - ' +
+    AMensagem
+  );
+end;
 
 procedure TfrmPrincipal.btnProcurarPastaClick(Sender: TObject);
 var
@@ -61,6 +77,9 @@ begin
 end;
 
 procedure TfrmPrincipal.btnAnalisarClick(Sender: TObject);
+var
+ Xml: TXmlInfo;
+
 begin
   if edtPasta.Text = '' then
   begin
@@ -68,12 +87,60 @@ begin
     Exit;
   end;
 
+  mmLog.Clear;
+  Log('Iniciando análise...');
+
   FreeAndNil(FListaXmls);
 
   FListaXmls := FScannerService.LerPasta(edtPasta.Text);
 
-  mmL
+   mmLog.Lines.Add(
+    Format('%d arquivo(s) encontrado(s).',
+      [FListaXmls.Count])
+  );
 
+  mmLog.Lines.Add('');
+  for Xml in FListaXmls do
+  begin
+   Log('Xml encontrado: ' + ExtractFileName(Xml.ArquivoOriginal));
+   Log('Chave encontrada: ' + Xml.ChaveDeAcesso);
+   Log('Novo nome: ' + Xml.NovoNome);
+    mmLog.Lines.Add('');
+  end;
+
+end;
+
+procedure TfrmPrincipal.btnRenomearClick(Sender: TObject);
+var
+ Xml : TXmlInfo;
+
+begin
+   if not Assigned(FListaXmls) then
+  begin
+    ShowMessage('Analise os arquivos primeiro.');
+    Exit;
+  end;
+
+  mmLog.Lines.Add('');
+  Log('===== RENOMEANDO =====');
+  mmLog.Lines.Add('');
+
+ for Xml in FListaXmls do
+ begin
+   try
+     FRenameService.Renomear(Xml);
+
+     Log('[OK] ' + ExtractFileName(Xml.ArquivoOriginal));
+
+   except
+     on E: Exception do
+      Log('[ERRO] ' + E.Message);
+   end;
+
+ end;
+
+ mmLog.Lines.Add('');
+ Log('Processo concluído.');
 end;
 
 end.
